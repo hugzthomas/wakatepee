d871540d4a98ac075d1f9d1bf62fc578289d5804
Justine Courtois <justine.crts@gmail.com>
2017-02-20 21:22:18 +0100

    Added transparent alerts

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   // navbar transition jQuery script
   $(window).scroll(function(e){
     if ($(this).scrollTop() > 0) {
-      $(".navbar-wagon").css({
+      $(".navbar-wagon-transparent").css({
         "background": "white"
       });
       $(".navbar-wagon-transparent-link").css({
@@ -10,7 +10,6 @@ $(document).ready(function() {
       });
       $(".navbar-wagon-brand").css({
         "color": "#333333",
-        "border-right": "1px solid #333333"
       });
       $(".dropdown-toggle").css({
         "color": "#333333",
@@ -21,15 +20,11 @@ $(document).ready(function() {
       $(".navbar-wagon-transparent").css({
         background: "transparent",
       });
-      $(".navbar-wagon").css({
-        background: "transparent",
-      });
       $(".navbar-wagon-transparent .navbar-wagon-transparent-link").css({
         "color": "white"
       });
       $(".navbar-wagon-brand").css({
         "color": "white",
-        "border-right": "1px solid white"
       });
       $(".dropdown-toggle").css({
         "color": "white",
