f139a6a2ad765369ebe9929f5c32419674776a7c
Quentin Casaubon <quentincasaubonweb@gmail.com>
2017-02-21 10:20:52 +0100

    new assocations on models and draft of pundit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,50 @@
+class ProjectsController < ApplicationController
+before_action :set_project, only: [:show, :edit, :update]
+
+  def index
+    @projects = policy_scope(Project) # Restaurant.all
+  end
+
+  def show
+    authorize(@project)
+  end
+
+  def new
+    @project = Project.new
+    authorize(@project)
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.admin = current_user
+    authorize(@project)
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+     if @project.update(project_params)
+      redirect_to @project
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :deadline)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :projects, through: :user_projects
   has_many :comments, dependent: :destroy
+  has_many :user_projects
 end
