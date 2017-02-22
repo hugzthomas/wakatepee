e32a22a838be0f41b8c2a7aaf32f2e81e0edb73a
hugz thomas <hugues.lam.thomas@gmail.com>
2017-02-22 17:27:03 +0100

    google connect

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
 gem 'pundit'
+gem "omniauth-google-oauth2"
 
 gem 'cloudinary', '1.1.7'
 gem 'attachinary', github: 'assembler/attachinary'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2]
   has_many :user_projects
   has_many :projects, through: :user_projects
   has_many :comments, dependent: :destroy
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+    user = User.create!(first_name: data["name"].split(" ").first,
+     email: data["email"],
+     remote_photo_url: data["image"],
+     password: Devise.friendly_token[0,20]
+    )
+  end
 end
+
+#     user
+# end
