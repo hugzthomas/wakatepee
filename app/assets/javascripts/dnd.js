cf20327c2aa80b706f200ad559b420223247d1fc
Justine Courtois <justine.crts@gmail.com>
2017-02-27 16:39:54 +0100

    Fixed issues with drag and drop and others js files

--- app/assets/javascripts/dnd.js
+++ app/assets/javascripts/dnd.js
@@ -1,27 +1,28 @@
-var el = document.getElementById('items');
-var sortable = Sortable.create(el,{
-    group: "localStorage-example",
-    store: {
-        /**
-         * Get the order of elements. Called once during initialization.
-         * @param   {Sortable}  sortable
-         * @returns {Array}
-         */
-        get: function (sortable) {
-            var order = localStorage.getItem(sortable.options.group.name);
-            return order ? order.split('|') : [];
-        },
+$(document).ready(function(){
+  if ($("#items").length) {
+    var el = document.getElementById('items');
+    var sortable = Sortable.create(el,{
+        group: "localStorage-example",
+        store: {
+            /**
+             * Get the order of elements. Called once during initialization.
+             * @param   {Sortable}  sortable
+             * @returns {Array}
+             */
+            get: function (sortable) {
+                var order = localStorage.getItem(sortable.options.group.name);
+                return order ? order.split('|') : [];
+            },
 
-        /**
-         * Save the order of elements. Called onEnd (when the item is dropped).
-         * @param {Sortable}  sortable
-         */
-        set: function (sortable) {
-            var order = sortable.toArray();
-            localStorage.setItem(sortable.options.group.name, order.join('|'));
+            /**
+             * Save the order of elements. Called onEnd (when the item is dropped).
+             * @param {Sortable}  sortable
+             */
+            set: function (sortable) {
+                var order = sortable.toArray();
+                localStorage.setItem(sortable.options.group.name, order.join('|'));
+            }
         }
-    }
+    });
+  }
 });
-
-
-
