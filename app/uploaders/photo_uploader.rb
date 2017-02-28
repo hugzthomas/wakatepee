a07e74d99833b3af655ede34aa6b8584b365d12a
maxime le maitre <maximelm25@gmail.com>
2017-02-28 19:46:25 +0100

    add back lines

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,11 +2,11 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
-  # process eager: true  # Force version generation at upload time.
+  process eager: true  # Force version generation at upload time.
 
-  # process convert: 'jpg'
+  process convert: 'jpg'
 
-  # version :standard do
-  #   resize_to_fit 800, 600
-  # end
+  version :standard do
+    resize_to_fit 800, 600
+  end
 end
