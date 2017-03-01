e765521cfe0238f67aaab966dd13703d49d2aaaa
Justine Courtois <justine.crts@gmail.com>
2017-03-01 18:44:54 +0100

    Added css to show project

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.display-comment').click(function(event) {
       $('.submilestone-card').css('border', 'none');
-      $(this).parent().parent().parent().css('border', '1px solid #FD7F6B');
+      // $(this).parent().parent().parent().css('border', '1px solid #FD7F6B');
       var id = $(this).attr("data-target");
       $('.card-comment').addClass('hidden');
       // console.log(id);
